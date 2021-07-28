b8266a8855124a4a1788576be211da58f9731699
bogdzn <wontsleep@protonmail.com>
2021-07-28 12:04:35 +0200

    (mod/chess-move-validation)light refactoring

--- src/chess_ai_validation.py
+++ src/chess_ai_validation.py
@@ -9,22 +9,13 @@ import sys
 
 class chess_game_analyzer:
     def __init__(self):
-        self.engine = None
         self.board = chess.Board()
-
-        if os.path.isfile("/bins/stockfish"):
-           self.engine = chess.engine.SimpleEngine.popen_uci("/bins/stockfish")
-
+        self.engine = chess.engine.SimpleEngine.popen_uci("/bins/stockfish")
 
     def __del__(self):
-        if self.engine != None:
-            await self.engine.quit()
+        await self.engine.quit()
 
-    async def analyse_position(self, fen_string, file_handle):
-        self.board = Board(fen_string)
-
-        if self.engine == None:
-            print("No engine loaded, please use the docker image provided in the repository")
-        else:
-            info = self.engine.analyse(self.board, chess.engine.Limit(depth=20))
-            print("fen:", fen_string, " - [", info.get("score"),"] -> ", info.get("pv")[0].uci)
+    async def analyse_position(self, fen_string):
+        self.board = chess.Board(fen_string)
+        info = self.engine.analyse(self.board, chess.engine.Limit(depth=20))
+        print("fen:", fen_string, " - [", info.get("score"),"] -> ", info.get("pv")[0].uci)
