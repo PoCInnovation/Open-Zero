4beede3b7fcd1a25eb1c22e2635be1a0cb4aaf8b
Gino AMBIGAIPALAN <gino.ambigaipalan@epitech.eu>
2021-07-31 15:43:42 +0200

    feat: chess_a3c.py is now split in different parts

--- src/chess_a3c.py
+++ src/chess_a3c.py
@@ -9,6 +9,8 @@ import gym_chess
 import threading
 import multiprocessing
 from typing import Optional
+from sharedadam import SharedAdam 
+from categoricalmasked import CategoricalMasked 
 
 import asyncio
 import chess
@@ -19,34 +21,6 @@ CUDA = False
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
-
-class CategoricalMasked(Categorical):
-
-    def __init__(self, logits: T.Tensor, mask: Optional[T.Tensor] = None):
-        self.mask = mask
-        self.batch, self.nb_action = logits.size()
-        if mask is None:
-            super(CategoricalMasked, self).__init__(logits=logits)
-        else:
-            self.mask_value = T.finfo(logits.dtype).min
-            logits.masked_fill_(~self.mask, self.mask_value)
-            super(CategoricalMasked, self).__init__(logits=logits)
-
-class SharedAdam(T.optim.Adam):
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.99), eps=1e-8,
-            weight_decay=0):
-        super(SharedAdam, self).__init__(params, lr=lr, betas=betas, eps=eps,
-                weight_decay=weight_decay)
-
-        for group in self.param_groups:
-            for p in group['params']:
-                state = self.state[p]
-                state['step'] = 0
-                state['exp_avg'] = T.zeros_like(p.data)
-                state['exp_avg_sq'] = T.zeros_like(p.data)
-
-                state['exp_avg'].share_memory_()
-                state['exp_avg_sq'].share_memory_()
 
 class ActorCritic(nn.Module):
     def __init__(self, input_dims, n_actions, gamma=0.99):
