5c6ef005e8e0ab27c62356c0ea4d47d6c4affa91
bogdzn <wontsleep@protonmail.com>
2021-08-12 21:13:21 +0200

    (mod/validation)set up engine checking but still has a few issues

--- src/solo_play.py
+++ src/solo_play.py
@@ -4,21 +4,37 @@ import gym
 import sys
 import socket
 import time
+import asyncio
 import numpy as np
 
-if __name__ == '__main__':
+async def analyse() -> None:
+    print('[SERVER] setting up tester...')
+
     network = ActorCritic(7616, 4672)
     env = gym.make('ChessAlphaZero-v0')
+    env_chess = gym.make('Chess-v0')
+    board = env_chess.reset()
 
     observation = env.reset()
-    done = False
-    reward = 0
-    while not done:
-        fen = env.env.env._board.fen()
-        actions = env.legal_actions
-        action = network.choose_action(np.array(observation).flatten(), actions)
-        _, _, done, _ = env.step(action)
-        print(f"{fen};{action}")
-        time.sleep(1)
+    _, engine = await chess.engine.popen_uci("/bins/stockfish")
 
+    print('[SERVER] ready to start...')
+    while not env.env.env._board.is_game_over():
+        fen = env.env.env._board.fen()
+        action = network.choose_action(np.array(observation).flatten(), env.legal_actions)
+        env.step(action)
+
+        board = chess.Board(fen)
+        with await engine.analyse(board, limit=20) as info:
+
+            uci_stockfish = info.get("pv")[0].uci
+            if uci_stockfish != env_chess.decode(action):
+                print(f'{fen};STOCKFISH: {info.get("pv")[0].uci};OPENZERO:{env.decode(action)}')
+
+    env_chess.close()
     env.close()
+    await engine.quit()
+
+if __name__ == '__main__':
+    asyncio.set_event_loop_policy(chess.engine.EventLoopPolicy())
+    asyncio.run(analyse())
